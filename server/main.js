e22cec4f0dda7a64fc57e57afbe7950ee6c4ff89
Frederico Maia Arantes <frederico.maia@tecsinapse.com.br>
2016-11-15 22:12:35 -0200

    - Adicionando alguns comentarios e links

--- server/main.js
+++ server/main.js
@@ -4,8 +4,12 @@ import { Votacao } from '../imports/api/votacao.js';
 import '../imports/api/publications.js';
 
 Meteor.startup(() => {
-  // code to run on server at startup
 
+  /**
+   * Utilizando Restivus para expor a collection Votacao via REST
+   * Apenas com essas linhas de código são gerados GET, POST na URL /api/votacao
+   * e GET, PUT, DELETE na URL /api/votacao/:id para os itens do banco
+   */
   const Api = new Restivus({
     useDefaultAuth: true,
     prettyJson: true
